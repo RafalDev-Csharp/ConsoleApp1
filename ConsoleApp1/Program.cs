4a4fb71b4727774d7ef4618f8dd968a6b2bfb8f8
RafalDev-Csharp <64135312+RafalDev-Csharp@users.noreply.github.com>
2025-11-28 17:47:50 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        
+        public static void Main()
+        {
+            
+        }
+    }
+
+
+    
+}

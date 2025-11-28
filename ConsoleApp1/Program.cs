10c26540618b5f061dab0b926426977509ef0383
RafalDev-Csharp <64135312+RafalDev-Csharp@users.noreply.github.com>
2025-11-28 18:22:38 +0100

    Add new class for calculations

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -14,8 +14,14 @@ namespace ConsoleApp1
         
         public static void Main()
         {
-            
-        }
+            int number = 3;
+            string result = number switch
+            {
+                1 => "One",
+                2 => "Two",
+                3 => "Three",
+                _ => "Unknown"
+            };
     }
 
 

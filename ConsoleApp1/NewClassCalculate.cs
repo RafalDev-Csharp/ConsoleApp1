27a5b1f4c9b9ae4c0a7879d92d20ce965ef5e556
RafalDev-Csharp <64135312+RafalDev-Csharp@users.noreply.github.com>
2025-11-30 15:26:33 +0100

    InitCOm on cleanBranch

---
+++ ConsoleApp1/NewClassCalculate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class NewClassCalculate
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double CalculateArea(double radius) => Math.PI * radius * radius;
+        public string City { get; set; }
+        public string Weatherty { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+
+        public static void Main()
+        {
+            int number = 3;
+            string result = number switch
+            {
+                1 => "One",
+                2 => "Two",
+                3 => "Three",
+                _ => "Unknown"
+            };
+
+            string str = "asdasd";
+        }
+
+        public void DissplayMsg()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
